c295708835e783f00eec72b15fe8d058ec87c5d1
Tiffany Arellano <tiffany.arellano@outlook.com>
2018-06-04 01:52:43 -0700

    display drawn name without 'x' at the beginning

--- script.js
+++ script.js
@@ -17,11 +17,17 @@ var $ = function (selector) {
 names.addEventListener("keyup", function(event){
 
         if (event.keyCode === 13) {
-            var li = document.createElement('li');
             var names = document.getElementById("names").value;
+            var li = document.createElement('li');
+            var span = document.createElement('span');
+                span.innerHTML = 'x';
+                span.className = 'deleteThis';
+            var sDel = '<span class="deleteThis">x</span>';
             if (textBox.value!==''){
-                li.innerHTML = '<span class="deleteThis">x</span>' + names;
+                
+                li.innerHTML = sDel + names;
                 ul.appendChild(li);
+
                 
                 var list = $('#list').getElementsByTagName('span');
                 for(var i = 0; i < list.length; i++){
@@ -29,7 +35,7 @@ names.addEventListener("keyup", function(event){
                     listItem.onclick = deleteThisFunc;
 
                 }
-
+console.log("names", names);
                 console.log(ul.innerText);
             } else {
                 console.log("nothing entered");
@@ -56,7 +62,8 @@ pickBtn.addEventListener("click", function(){
     
     // -- DISPLAY PICKED NAME --
     $('#pickedTitle').style.display = "none";
-    pickedDisplay.innerText = pickedLi;
+    // pickedDisplay.innerText = pickedLi;
+    pickedDisplay.innerText = pickedLi.substring(1);
 
     // -- CONSOLE TESTS --
     console.log("picked name", pickedLi);
