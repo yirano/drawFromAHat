cee1ab4825f42346e0fe6b2d6f0caad3ede4a36b
dhuang612 <dhuang@madisoncollege.edu>
2019-09-03 12:57:31 -0400

    added in design changes

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ names.addEventListener('keyup', function(event) {
   if (event.keyCode === 13) {
     var names = document.getElementById('names').value;
     var li = document.createElement('li');
-<<<<<<< HEAD
     li.contentEditable = 'true';
     var span = document.createElement('span');
     span.innerHTML = 'x';
@@ -33,15 +32,6 @@ names.addEventListener('keyup', function(event) {
         var listItem = list[i];
         listItem.addEventListener('click', deleteThisFunc);
       }
-=======
-    // li.contentEditable = 'true';
-    var sDel =
-      '<button class="deleteThis" onclick="deleteThisFunc(this)">x</button>';
-    let name = `<input type="text" class="name" value="${names}"></input>`;
-    if (textBox.value !== '') {
-      li.innerHTML = sDel + name;
-      ul.appendChild(li);
->>>>>>> gh-pages
 
       console.log('names', names);
       console.log(ul.innerText);
@@ -60,7 +50,6 @@ names.addEventListener('keyup', function(event) {
 pickBtn.addEventListener('click', function() {
   // find the length of li elements in ul
   var liLength = ul.getElementsByTagName('li').length;
-<<<<<<< HEAD
   var randomNum = Math.floor(Math.random() * liLength);
   var pickedLi = ul.getElementsByTagName('li')[randomNum].innerText;
 
@@ -74,27 +63,6 @@ pickBtn.addEventListener('click', function() {
   console.log('pick button clicked');
 
   // this.disabled = true;
-=======
-  let pickedLi;
-  if (liLength == 0) {
-    pickedDisplay.innerHTML.value = 'Name drawn will show here';
-    pickedDisplay.style.color = 'rgb(77, 77, 77)';
-  } else {
-    var randomNum = Math.floor(Math.random() * liLength);
-    pickedLi = ul.getElementsByTagName('li')[randomNum].children[1].value;
-    // -- DISPLAY PICKED NAME --
-    $('#pickedTitle').style.display = 'none';
-    // pickedDisplay.innerText = pickedLi;
-    pickedDisplay.innerText = pickedLi;
-    pickedDisplay.style.color = 'rgb(21, 116, 71)';
-    // -- CONSOLE TESTS --
-    console.log('picked name', pickedLi);
-    console.log('pick button clicked');
-
-    confetti.start(1100, 50, 550);
-    // this.disabled = true;
-  }
->>>>>>> gh-pages
 });
 
 pickedDisplay.addEventListener('click', function() {
@@ -105,17 +73,10 @@ pickedDisplay.addEventListener('click', function() {
 // ********* CLEAR LIST *********
 clearListBtn.addEventListener('click', function(event) {
   ul.innerHTML = '';
-<<<<<<< HEAD
 
   $('#pickedTitle').style.display = 'block';
   pickedDisplay.innerText = '';
 
-=======
-
-  $('#pickedTitle').style.display = 'block';
-  pickedDisplay.innerText = '';
-
->>>>>>> gh-pages
   // -- CONSOLE TESTS --
   console.log('clear list button clicked');
 });
@@ -123,7 +84,6 @@ clearListBtn.addEventListener('click', function(event) {
 // ************ FUNCTIONS ************
 
 // ---- DELETE THIS FUNCTION ----
-<<<<<<< HEAD
 function deleteThisFunc() {
   this.parentNode.remove(this);
 }
@@ -142,23 +102,3 @@ document.getElementById('add').addEventListener('click', function() {
 
   console.log('add name button clicked');
 });
-=======
-function deleteThisFunc(e) {
-  e.parentNode.remove();
-}
-
-document.getElementById('add').onclick = function() {
-  var names = document.getElementById('names').value;
-
-  if (textBox.value !== '') {
-    li.innerHTML = names;
-    ul.appendChild(li);
-  } else {
-    console.log('nothing entered');
-  }
-
-  document.getElementById('names').value = '';
-
-  console.log('add name button clicked');
-};
->>>>>>> gh-pages
