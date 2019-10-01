872b402da7d6bd0a0d2e6a1e379bd081b0cac530
GiraffeKey <giraffekee@gmail.com>
2019-09-30 21:35:21 -0700

    fix add to list button functionality
    There was a function at the bottom of the add to list button that did a completely different thing than pressing enter for some reason.

--- src/script.js
+++ src/script.js
@@ -8,36 +8,42 @@ var pickBtn = document.getElementById('pick');
 var textBox = document.getElementById('names');
 var pickedDisplay = document.getElementById('pickedDisplay');
 var deleteThis = document.getElementsByClassName('deleteThis');
-var li = document.createElement('li');
 var $ = function (selector) {
   return document.querySelector(selector);
 };
 
 // ********* ADD LI ELEMENT TO UL *********
-names.addEventListener('keyup', function (event) {
-  if (event.keyCode === 13) {
-    var names = document.getElementById('names').value;
-    var li = document.createElement('li');
-    // li.contentEditable = 'true';
-    var sDel =
-      '<button class="deleteThis" onclick="deleteThisFunc(this)">x</button>';
-    let name = `<input type="text" class="name" value="${names}"></input>`;
-    if (textBox.value !== '') {
-      li.innerHTML = sDel + name;
-      ul.appendChild(li);
+function addToList() {
+  var names = document.getElementById('names').value;
+  var li = document.createElement('li');
+  // li.contentEditable = 'true';
+  var sDel = '<button class="deleteThis" onclick="deleteThisFunc(this)">x</button>';
+  let name = `<input type="text" class="name" value="${names}"></input>`;
+  if (textBox.value !== '') {
+    li.innerHTML = sDel + name;
+    ul.appendChild(li);
 
-      console.log('names', names);
-      console.log(ul.innerText);
-    } else {
-      console.log('nothing entered');
-    }
-
-    document.getElementById('names').value = '';
-
-    // -- CONSOLE TESTS --
-    console.log('enter pressed');
+    console.log('names', names);
+    console.log(ul.innerText);
+  } else {
+    console.log('nothing entered');
   }
+
+  document.getElementById('names').value = '';
+
+  // -- CONSOLE TESTS --
+  console.log('enter pressed');
+}
+
+// Press enter in input field
+names.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) addToList();
 });
+// Click on Add to List button
+document.getElementById('add').onclick = function() {
+  addToList();
+}
+// (I couldn't get it to work properly by just setting the onclick to addToList() for some reason, don't know why)
 
 // ************ PICK RANDOM FROM LIST ************
 pickBtn.addEventListener('click', function () {
@@ -51,7 +57,7 @@ pickBtn.addEventListener('click', function () {
     var randomNum = Math.floor(Math.random() * liLength);
     pickedLi = ul.getElementsByTagName('li')[randomNum].children[1].value;
     // -- DISPLAY PICKED NAME --
-    $('#pickedTitle').style.display = 'none';
+    document.getElementById("pickedTitle").style.display = 'none';
     // pickedDisplay.innerText = pickedLi;
     pickedDisplay.innerText = pickedLi;
     pickedDisplay.style.color = 'rgb(21, 116, 71)';
@@ -85,18 +91,3 @@ clearListBtn.addEventListener('click', function (event) {
 function deleteThisFunc(e) {
   e.parentNode.remove();
 }
-
-document.getElementById('add').onclick = function () {
-  var names = document.getElementById('names').value;
-
-  if (textBox.value !== '') {
-    li.innerHTML = names;
-    ul.appendChild(li);
-  } else {
-    console.log('nothing entered');
-  }
-
-  document.getElementById('names').value = '';
-
-  console.log('add name button clicked');
-};
